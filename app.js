e30af3b39fbb9845de55b359ecdec60a5a401477
Daniel Dakhno <dakhnod@gmail.com>
2023-03-21 16:27:38 +0100

    added argument parsing and database layer

--- app.js
+++ app.js
@@ -1,32 +1,153 @@
-const jsonata = require('jsonata');
-const fs = require('fs');
+import jsonata from 'jsonata';
+import fs from 'fs';
+import axios from 'axios';
+import {default as AxiosDigestAuth} from '@mhoc/axios-digest-auth';
+import { exec } from 'child_process';
+import sqlite3 from 'sqlite3';
+import { parseArgs } from 'node:util'
 
 (async () => {
-    const files = []
+    const args = parseArgs({
+        options: {
+            database: {
+                type: 'string'
+            },
+            files: {
+                type: 'string',
+                multiple: true,
+            }
+        }
+    })
+
+    if(args.values.files == undefined){
+        throw "missing --files parameter"
+    }
+    const files = args.values.files
+
+    if(args.values.database != undefined){
+        let database = new sqlite3.Database(args.values.database)
+    }
 
     let http_args_default = {
         method: 'GET'
     }
 
-    const bindings = {
-        http_default_args_set: (args) => http_args_default = args,
-        http_request: (args) => {
-            args = Object.assign(http_args_default, args)
-        },
-        db_row_exists: (table, cols) => true,
-        db_row_create: (table, cols) => true,
+    function bindings_to_selector(bindings){
+        return Object.keys(bindings).map(key => key + '=?').join(' AND ')
     }
 
-    const parseFile = (files, value={}) => {
-        if(files.length == 0) return
+    function bindings_to_values(bindings){
+        return Object.values(bindings)
+    }
+
+    function bindings_to_insert_values(bindings){
+        return Array(Object.values(bindings).length).fill('?').join(',')
+    }
+
+    function assert_database_exists(){
+        if(database == undefined){
+            throw 'in order to use database specify --database argument'
+        }
+    }
+
+    const bindings = 
+    {
+        core: {
+            http: {
+                default_opts: {},
+                request: async (options) => {
+                    let requester = axios
+                    if(options.digest){
+                        requester = new AxiosDigestAuth.default(options.digest)
+                    }
+                    try{
+                        let response = await requester.request(options)
+
+                        if(options.pagination){
+                            const result = response.data.data
+
+                            for(let current_page = 2; current_page <= response.data.pagination.page_last; current_page++){
+                                const pagination = response.data.pagination
+
+                                options.params = Object.assign({}, options.params, {page: current_page})
+
+                                response = await requester.request(options)
+
+                                result.push(...response.data.data)
+                            }
+
+                            response.data = result
+                        }
+
+                        return response
+                    }catch(e){
+                        console.error(e)
+                    }
+                }
+            },
+            'db': {
+                table_assert: async (table, ...cols) => {
+                    assert_database_exists()
+                    const sql = `CREATE TABLE IF NOT EXISTS ${table}(${cols.join(',')})`
+                    await new Promise((resolve, reject) => {
+                        database.run(sql, (err) => {
+                            if(err != null){
+                                reject(err)
+                                return
+                            }
+                            resolve()
+                        })
+                    })
+                },
+                row_exists: async (table, cols) => {
+                    assert_database_exists()
+                    const sql = `SELECT * FROM ${table} WHERE ${bindings_to_selector(cols)}`
+
+                    const result = await new Promise((resolve, reject) => {
+                        database.get(sql, bindings_to_values(cols), (err, row) => {
+                            if(err != null){
+                                reject(err)
+                                return
+                            }
+                            resolve(row)
+                        })
+                    })
+                    return result != undefined
+                },
+                row_create: async (table, cols) => {
+                    assert_database_exists()
+                    const sql = `INSERT INTO ${table}(${Object.keys(cols)}) VALUES (${bindings_to_insert_values(cols)})`
+
+                    await new Promise((resolve, reject) => {
+                        database.run(sql, bindings_to_values(cols), (err) => {
+                            if(err != null){
+                                reject(err)
+                                return
+                            }
+                            resolve()
+                        })
+                    })
+                },
+            },
+            'console': console
+        }
+    }
+
+    const parseFile = async (files, value={}) => {
+        if(files.length == 0) return value
 
         const content = fs.readFileSync(files[0], 'utf8');
         const expression = jsonata(content)
 
-        const result = jsonata.evaluate(value, bindings)
+        const result = await expression.evaluate(value, bindings)
 
-        return parseFile(files.slice(1))
+        return await parseFile(files.slice(1), result)
     }
 
-    parseFile(files[0])
+    try{
+        const result = await parseFile(files)
+        console.log(result)
+    }catch(e){
+        console.log(e)
+    }
 })()
